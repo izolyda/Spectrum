2b3e44dd82887b0d7d912d4bbb382532a4a5f75c
Izolyda <izolyda@gmail.com>
2022-06-16 19:20:06 -0700

    Icons, text adjustments

---
+++ Spectrum/Spectrum.Android/CustomRenderers/CustomLabelRenderer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Android.Content;
+using Spectrum.Droid.CustomRenderers;
+using Spectrum.Helpers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+
+[assembly: ExportRenderer(typeof(Label), typeof(CustomLabelRenderer))]
+
+namespace Spectrum.Droid.CustomRenderers
+{
+	public class CustomLabelRenderer : LabelRenderer
+	{
+		public CustomLabelRenderer(Context context): base(context)
+		{
+		}
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+        {
+            base.OnElementChanged(e);
+
+            var el = Element as CustomLabel;
+
+            if (el != null && el.JustifyText)
+            {
+                if (Android.OS.Build.VERSION.SdkInt >= Android.OS.BuildVersionCodes.O)
+                {
+                    Control.JustificationMode = Android.Text.JustificationMode.InterWord;
+                }
+
+            }
+        }
+    }
+
+   
+}
+
---
+++ Spectrum/Spectrum/Helpers/CustomLabel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Spectrum.Helpers
+{
+    public class CustomLabel : Label
+    {
+        public static readonly BindableProperty JustifyTextProperty =
+            BindableProperty.Create(
+                propertyName: nameof(JustifyText),
+                returnType: typeof(Boolean),
+                declaringType: typeof(CustomLabel),
+                defaultValue: false,
+                defaultBindingMode: BindingMode.OneWay
+         );
+
+        public bool JustifyText
+        {
+            get { return (Boolean)GetValue(JustifyTextProperty); }
+            set { SetValue(JustifyTextProperty, value); }
+        }
+    }
+}
+
